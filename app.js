68b7d5e1f58d5ffe39c8391c35f7fdd11a1f692e
ikemarcus <ikemarcus@hotmail.nl>
2024-05-17 10:21:05 +0200

    v1

---
+++ app.js
@@ -0,0 +1,65 @@
+const steden = [
+    // Nederlandse steden
+    "Amsterdam", "Rotterdam", "Utrecht", "Den Haag", "Eindhoven",
+    "Maastricht", "Groningen", "Leiden", "Delft", "Arnhem",
+    "Nijmegen", "Haarlem", "Breda", "Tilburg", "Zwolle",
+    "Alkmaar", "Amersfoort", "Apeldoorn", "Den Bosch", "Enschede",
+    "Leeuwarden", "Middelburg", "Roermond", "Venlo", "Vlissingen",
+    "Zoetermeer", "Zandvoort", "Hengelo", "Heerlen", "Hilversum",
+    "Dordrecht", "Oss", "Veenendaal", "Hoofddorp", "Houten",
+    "Emmen", "Assen", "Helmond", "Sittard", "Hoorn",
+    "Ede", "Gouda", "Almere", "Schiedam", "Katwijk",
+    "Vlissingen", "Terneuzen", "Bergen op Zoom", "Gorinchem", "Hoogeveen",
+    "Lelystad", "Heerhugowaard", "Capelle aan den IJssel", "Nieuwegein", "Vlaardingen",
+    "Spijkenisse", "Deventer", "Zeist", "Lisse", "Wageningen",
+
+    // Belgische steden
+    "Brussel", "Antwerpen", "Gent", "Brugge", "Leuven",
+    "Luik", "Mechelen", "Kortrijk", "Hasselt", "Oostende",
+    "Namen", "Mons", "Aalst", "Turnhout", "Sint-Niklaas",
+    "Genk", "Roeselare", "Tienen", "Tongeren", "Dendermonde",
+    "Ieper", "Lier", "Lokeren", "Vilvoorde", "Heist-op-den-Berg",
+    "Herentals", "Knokke-Heist", "Maasmechelen", "Aarschot", "Ninove"
+];
+
+const activiteiten = [
+    "een museum bezoeken", "een fietstocht maken", "een wandeling maken in een park",
+    "een historische tour doen", "een boottocht maken", "winkelen in het stadscentrum",
+    "eten in een lokaal restaurant", "een concert bijwonen", "een theater bezoeken",
+    "een fotografie tour doen", "een kunstgalerie bezoeken", "picknicken",
+    "een dierentuin bezoeken", "uitgaan naar een bar of club", "een lokale markt bezoeken",
+    "een workshop volgen", "een festival bezoeken", "kajakken of kanoën",
+    "een botanische tuin bezoeken", "een kasteel of paleis bezoeken",
+    "een aquarium bezoeken", "een pretpark bezoeken", "een speurtocht doen",
+    "een escape room doen", "een proeverij van lokale producten doen",
+    "een brouwerij of wijngaard bezoeken", "een sportevenement bezoeken",
+    "een strand bezoeken", "een natuurreservaat bezoeken", "sterrenkijken",
+    "een fotoworkshop volgen", "een street art tour doen", "een workshop pottenbakken volgen",
+    "een workshop schilderen volgen", "een cooking class volgen",
+    "een dansles volgen", "yoga in het park doen", "een planetarium bezoeken",
+    "een waterpark bezoeken", "een spa of wellnesscentrum bezoeken",
+    "een workshop fotografie volgen", "een dierenpark bezoeken",
+    "wandelen door een historisch centrum", "een lokale brouwerij bezoeken",
+    "een lokaal theater bezoeken", "een windmolen bezoeken",
+    "een lokale boerderij bezoeken", "een carnavalsoptocht bezoeken",
+    "een proeverij van kaas en wijn doen", "een chocolademuseum bezoeken",
+    "een kookworkshop volgen", "een kerstmarkt bezoeken",
+    "een muziekfestival bijwonen", "een jazzclub bezoeken",
+    "sightseeing met een hop-on-hop-off bus doen", "een historische begraafplaats bezoeken",
+    "een openluchtmuseum bezoeken", "een lokale parade bezoeken",
+    "wandelen langs de grachten", "een lokale vismarkt bezoeken",
+    "een dagtocht naar de kust doen", "een proeverij van Belgische bieren doen",
+    "een culinaire tour doen"
+];
+
+function randomItem(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+
+function genereerStadEnActiviteit() {
+    const randomStad = randomItem(steden);
+    const randomActiviteit = randomItem(activiteiten);
+    const resultaat = `Bezoek <span class="highlight">${randomStad}</span> en ga <span class="highlight">${randomActiviteit}</span>.`;
+    document.getElementById('resultaat').innerHTML = resultaat;
+}
